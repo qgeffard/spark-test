8bfc1986f946cc54ae183024dff901f40ae4c983
omahjoub <omahjoub.externe@m6.fr>
2016-06-29 11:26:39 +0200

    Initialisation Exo 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -162,34 +162,8 @@ public class Main {
         System.out.println();
         System.out.println("***** EXO 7 *****");
 
-        // Top 5 des validations par gare :
-        // On groupe par gare
-        validations.groupBy(v -> v.getLibelleArret())
-                // Pour chaque gare on calcule le total des validations
-                .mapToPair(tuple -> {
-                    Map<String, Long> result = new HashMap<>();
-                    result.put(tuple._1(), 0L);
-                    tuple._2().forEach(validation -> {
-                        result.put(tuple._1(), result.get(tuple._1()) + validation.getValidations());
-                    });
-                    return new Tuple2<String, Long>(tuple._1(), result.get(tuple._1()));
-                })
-                        // On trie par ordre ascendant sur les validations et on garde le top 5
-                .top(5, MyTupleComparator.INSTANCE)
-                .stream().forEach(stringLongTuple2 -> System.out.println(stringLongTuple2._1() + " : " + stringLongTuple2._2() + " =====> " + ((double)  stringLongTuple2._2() / totalValidations) * 100 + " %"));
-
         System.out.println("*****  END  *****");
         System.out.println();
-    }
-
-    // Comparator sur les validations par gare
-    static class MyTupleComparator implements Comparator<Tuple2<String, Long>>, Serializable {
-
-        final static MyTupleComparator INSTANCE = new MyTupleComparator();
-
-        public int compare(Tuple2<String, Long> t1, Tuple2<String, Long> t2) {
-            return t1._2.compareTo(t2._2);    // sort descending
-        }
     }
 
 }
